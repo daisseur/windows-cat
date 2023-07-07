51ec2f71f4d228e802415f991cb7541551c5bcdf
daisseur <100715068+daisseur@users.noreply.github.com>
2023-07-07 14:00:51 +0200

    Update get_hashnt.sh

--- get_hashnt.sh
+++ get_hashnt.sh
@@ -16,8 +16,8 @@ sudo rmdir /mnt/$part
 # sudo apt-get install impacket-secretsdump -y > /dev/null
 hashs="$(sudo impacket-secretsdump -sam ./win/SAM -system ./win/SYSTEM -security ./win/SECURITY LOCAL | grep :::)"
 echo $hashs
-# Remplacer ':::' par un espace
-hashs=${hashs//:::/$'\n'/}
+
+hashs=${hashs// ::: /$'\n'/}
 echo $hashs
 
 # Remplacer ':' par un espace
